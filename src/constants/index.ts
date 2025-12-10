c8e0298f38cbb125034c8b1095bb7376a9111434
Steve Tomi <steveola23@gmail.com>
2025-12-10 20:51:03 +0100

    update deployment block

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,6 +12,6 @@ export async function getConsts(network: NetworkType, key = PaymentKey) {
 }
 
 export const paynestDaoFactory = "0x9a30D7f68095fe7506f12f62f188789374026593";
-export const deploymentBlock = 34033324;
+export const deploymentBlock = 37922894;
 export const USDC = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
 export const FEE_COLLECTOR = "0xa09902f06abfcf842cc129b9b71882c4481fad26";
